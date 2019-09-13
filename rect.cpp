7ba2f35a3830ffa7bce1652ad17379b06c0d93fe
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-09-13 18:40:41 +0000

    Initial code

---
+++ rect.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 myself All rights reserved.
+//
+// Created by: Matsuru Hoshi
+// Created on: Sept 2019
+// This is fancy hello
+
+#include <iostream>
+
+int main() {
+    std::cout << "The area of 5 by 3 is: " << (5 * 3) << std::endl;
+    std::cout << "The perimeter of 5 by 3 is: " << (2 * (5 + 3))  << std::endl;
+}
